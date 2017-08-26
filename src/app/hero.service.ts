7cac798942b7f56846b74daabe7bec958d6205cd
Damir Bogdanov <yespeaceee@gmail.com>
2017-08-26 18:54:46 +1000

    Part 5

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,11 +9,8 @@ export class HeroService {
     return Promise.resolve(HEROES);
   }
 
-  // See the "Take it slow" appendix
-  getHeroesSlowly(): Promise<Hero[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-      setTimeout(() => resolve(this.getHeroes()), 2000);
-    });
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
   }
 }
