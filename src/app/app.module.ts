ba8abeb406e5015eb0cdd9da80012cfacfb9bf0d
Damir Bogdanov <yespeaceee@gmail.com>
2017-08-26 17:07:26 +1000

    Part 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
